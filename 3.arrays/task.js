7869158c904d81f969760b03b07ec8f013c23fb2
Lidiya <simutina.1994@mail.ru>
2023-06-11 17:58:25 +0300

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,18 @@
 function compareArrays(arr1, arr2) {
-  
+	if (
+		arr1.length === arr2.length &&
+		arr1.every((element, index) => element === arr2[index])
+	) {
+		return true;
+	} else {
+		return false;
+	}
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	let result = users
+		.filter(user => user.gender === gender)
+		.map(user => user.age)
+		.reduce((sum, age, index, array) => (sum + age / array.length), 0);
+	return result;
 }
